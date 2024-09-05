046dfdaed93425f8103efd4975dfcc5027f8fcfc
chamod kavinda <kavindachamod100@gmail.com>
2024-09-05 20:01:50 +0530

    implement category service

---
+++ src/main/java/com/example/POS/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.example.POS.service;
+
+import com.example.POS.dto.CategoryDTOSave;
+
+public interface CategoryService {
+    String addCategory(CategoryDTOSave categoryDTOSave);
+}
---
+++ src/main/java/com/example/POS/service/IMPL/CategoryServiceIMPL.java
@@ -0,0 +1,13 @@
+package com.example.POS.service.IMPL;
+
+import com.example.POS.dto.CategoryDTOSave;
+import com.example.POS.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceIMPL implements CategoryService {
+    @Override
+    public String addCategory(CategoryDTOSave categoryDTOSave) {
+        return null;
+    }
+}
