a99bcde4e31db347af664a8621fb2aefd04c95b8
chamod kavinda <kavindachamod100@gmail.com>
2024-09-05 18:49:03 +0530

    create category entity

---
+++ src/main/java/com/example/POS/entity/category.java
@@ -0,0 +1,21 @@
+package com.example.POS.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="category")
+public class category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name="cat_id",length = 11)
+    private int catid;
+
+    @Column(name="cat_name",length = 45)
+    private String catname;
+
+    @Column(name="active",columnDefinition = "TINYINT default 1")
+    private boolean active;
+}
