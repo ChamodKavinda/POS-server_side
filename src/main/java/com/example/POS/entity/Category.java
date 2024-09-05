29d5f0d6dd6699af90e1d64e27dbc10a5526ede8
chamod kavinda <kavindachamod100@gmail.com>
2024-09-05 20:17:19 +0530

    implement getters and setters

--- src/main/java/com/example/POS/entity/Category.java
+++ src/main/java/com/example/POS/entity/Category.java
@@ -18,4 +18,52 @@ public class Category {
 
     @Column(name="active",columnDefinition = "TINYINT default 1")
     private boolean active;
+
+
+    public Category(int catid, String catname, boolean active) {
+        this.catid = catid;
+        this.catname = catname;
+        this.active = active;
+    }
+
+    public Category(String catname, boolean active) {
+        this.catname = catname;
+        this.active = active;
+    }
+
+    public Category() {
+    }
+
+    public int getCatid() {
+        return catid;
+    }
+
+    public void setCatid(int catid) {
+        this.catid = catid;
+    }
+
+    public String getCatname() {
+        return catname;
+    }
+
+    public void setCatname(String catname) {
+        this.catname = catname;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "catid=" + catid +
+                ", catname='" + catname + '\'' +
+                ", active=" + active +
+                '}';
+    }
 }
