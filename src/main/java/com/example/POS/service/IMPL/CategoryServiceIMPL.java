d57460c4f3e7b715814be5cb3b2aaef4fab3ae23
chamod kavinda <kavindachamod100@gmail.com>
2024-09-07 12:29:00 +0530

    save category service implemented

--- src/main/java/com/example/POS/service/IMPL/CategoryServiceIMPL.java
+++ src/main/java/com/example/POS/service/IMPL/CategoryServiceIMPL.java
@@ -1,13 +1,22 @@
 package com.example.POS.service.IMPL;
 
 import com.example.POS.dto.CategoryDTOSave;
+import com.example.POS.entity.Category;
+import com.example.POS.repo.CategoryRepo;
 import com.example.POS.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceIMPL implements CategoryService {
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
     @Override
     public String addCategory(CategoryDTOSave categoryDTOSave) {
-        return null;
+        Category category = new Category(categoryDTOSave.getCat_name(), categoryDTOSave.isActive());
+                categoryRepo.save(category);
+                return category.getCatname();
     }
 }
