3607dfe8345878ab6405707f7d1b3855116164a1
chamod kavinda <kavindachamod100@gmail.com>
2024-09-07 10:27:43 +0530

    implement constructors

--- src/main/java/com/example/POS/dto/CategoryDTO.java
+++ src/main/java/com/example/POS/dto/CategoryDTO.java
@@ -1,7 +1,13 @@
 package com.example.POS.dto;
 
-import jakarta.persistence.Column;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CategoryDTO {
     private int catid;
     private String catname;
