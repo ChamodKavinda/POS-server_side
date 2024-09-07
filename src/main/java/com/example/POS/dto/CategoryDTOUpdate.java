f5455513db13abb73fa8cdfd3737b96ebccc7df4
chamod kavinda <kavindachamod100@gmail.com>
2024-09-07 10:29:39 +0530

    implement Data using lombok

--- src/main/java/com/example/POS/dto/CategoryDTOUpdate.java
+++ src/main/java/com/example/POS/dto/CategoryDTOUpdate.java
@@ -1,5 +1,13 @@
 package com.example.POS.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CategoryDTOUpdate {
     private String catname;
     private boolean active;
